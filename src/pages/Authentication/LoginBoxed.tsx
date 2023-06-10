9f191185fca63c784c42e8ca7b79affd509bb924
Joshua <mernstackpor@gmail.com>
2023-06-10 02:27:14 -0500

    initial commit

---
+++ src/pages/Authentication/LoginBoxed.tsx
@@ -0,0 +1,41 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { IRootState } from '../../store';
+import { useEffect } from 'react';
+import { setPageTitle } from '../../store/themeConfigSlice';
+
+const LoginBoxed = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(setPageTitle('Login Boxed'));
+    });
+    const navigate = useNavigate();
+    const isDark = useSelector((state: IRootState) => state.themeConfig.theme) === 'dark' ? true : false;
+
+    const submitForm = () => {
+        navigate('/');
+    };
+
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-cover bg-center bg-[url('/assets/images/map.svg')] dark:bg-[url('/assets/images/map-dark.svg')]">
+            <div className="panel sm:w-[480px] m-6 max-w-lg w-full">
+                <h2 className="font-bold text-2xl mb-3">Sign In</h2>
+                <form className="space-y-5" onSubmit={submitForm}>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input id="email" type="email" className="form-input" placeholder="Enter Email" />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input id="password" type="password" className="form-input" placeholder="Enter Password" />
+                    </div>
+                    <button type="submit" className="btn btn-primary w-full">
+                        SIGN IN
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginBoxed;
